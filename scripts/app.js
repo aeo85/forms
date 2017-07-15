56113bdac38f0fcb36d937da69ccc8fe10d619e5
Amy <amynedervelt@gmail.com>
2017-07-15 09:52:43 -0500

    java homework
    3/5 - I completed the homework, but I don't really understand how I got there other than copy and paste and reformating the syntax to match what I needed it to do.
    
    3/5 - I don't think I'd no where to start if I didn't have the previous code to help guide me along.

--- scripts/app.js
+++ scripts/app.js
@@ -1,15 +1,90 @@
 console.log('hi');
 
+var firstSentence = 'Go to the Great Barrier Reef';
+var secondSentence = 'Go to Machu Picchu';
+var thirdSentence = 'You snorkeled all day';
+var fourthSentence = 'You hiked nonstop';
+var fifthSentence = 'And saw all kinds of tropical fish';
+var sixthSentence = 'You saw llamas and they were cool';
+var seventhSentence = 'You returned home sunburned';
+var eigthSentence = 'You returned home sick, bummer';
+
 window.onload = function(event) {
 
     var nameInput = document.getElementById('first_name');
     var nameBtn = document.getElementById('name_btn');
+    var firstChoice = document.getElementById('choice-one');
+    var secondChoice = document.getElementById('choice-two');
+    var thirdChoice = document.getElementById('choice-three');
+    var fourthChoice = document.getElementById('choice-four');
+    var fifthChoice = document.getElementById('choice-five');
+    var sixthChoice = document.getElementById('choice-six');
+    var seventhChoice = document.getElementById('choice-seven');
+    var eighthChoice = document.getElementById('choice-eight');
 
-    nameBtn.onclick = function(event){
-      // console.log(event);
-      console.log(nameInput.value);
+    firstChoice.onclick = function(event) {
+      createRow(firstSentence);
+      document.getElementById('first-step').style.display = 'none';
     }
 
-    console.log(nameInput, nameBtn);
+    secondChoice.onclick = function(event) {
+      createRow(secondSentence);
+      document.getElementById('first-step').style.display = 'none';
+    }
+
+    thirdChoice.onclick = function(event) {
+      createRow(thirdSentence);
+      document.getElementById('second-step').style.display = 'none';
+    }
+
+    fourthChoice.onclick = function(event) {
+      createRow(fourthSentence);
+      document.getElementById('second-step').style.display = 'none';
+    }
+
+    fifthChoice.onclick = function(event) {
+      createRow(fifthSentence);
+      document.getElementById('third-step').style.display = 'none';
+    }
+
+    sixthChoice.onclick = function(event) {
+      createRow(sixthSentence);
+      document.getElementById('third-step').style.display = 'none';
+    }
+
+    seventhChoice.onclick = function(event) {
+      createRow(seventhSentence);
+      document.getElementById('fourth-step').style.display = 'none';
+    }
+
+    eighthChoice.onclick = function(event) {
+      createRow(eighthSentence);
+      document.getElementById('fourth-step').style.display = 'none';
+    }
+
+  nameBtn.onclick = function(event) {
+    // console.log(event);
+    console.log(nameInput.value);
+    createRow('Welcome aboard, ' + nameInput.value);
+}
+      console.log(nameInput, nameBtn);
+
+}
+// this function creates and adds rows to our app
+  function createRow(words) {
+  console.log(words);
+  // we need a new div
+  var row = document.createElement('div');
+  var col = document.createElement('div');
+  var p = document.createElement('p');
+  p.innerHTML = words;
+  row.append(col);
+  row.className = 'row';
+  col.className = 'col s12';
+  col.append(p);
+  console.log(row);
+  // append the whole thing to app
+  var app = document.getElementById('app');
+  app.append(row);
 
 }
